241f9e56091065a866f4318c317cb3b48d79178b
Beatriz R. Montanini <montanini249@gmail.com>
2023-05-10 22:16:29 -0300

    um

---
+++ src/dois.js
@@ -0,0 +1,8 @@
+let entrada = 60
+;
+if( entrada < 18 ){
+ console.log("de menor");
+}
+else{
+ console.log("de maior");
+}
---
+++ src/um.js
@@ -0,0 +1,7 @@
+let entrada = "15";
+entrada = parseInt(entrada)
+;
+
+soma = entrada + entrada;
+
+console.log( soma );
